63e6bd84d4ed8af0239590ef0fa72a6ece6c44ac
Matt F <trumpetman072392@gmail.com>
2021-12-02 10:59:56 -0600

    can add operative/find most/least secure system

--- Bank.cs
+++ Bank.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bank_Heist_2
 {
@@ -11,6 +13,37 @@ namespace Bank_Heist_2
         public int VaultScore { get; set; }
 
         public int SecurityGuardScore { get; set; }
+
+        public string MostSecureSystem
+        {
+            get
+            {
+                return securityScores.OrderByDescending(score => score.Value).First().Key;
+            }
+        }
+
+        public string LeastSecureSystem
+        {
+            get
+            {
+                return securityScores.OrderBy(score => score.Value).First().Key;
+            }
+        }
+
+        private Dictionary<string, int> securityScores
+        {
+            get
+            {
+                return new Dictionary<string, int>
+                {
+                    {"Alarm Score", AlarmScore},
+                    {"Vault Score", VaultScore},
+                    {"Security Guard Score", SecurityGuardScore}
+                };
+            }
+
+        }
+
 
         public bool IsSecure
         {
--- Program.cs
+++ Program.cs
@@ -9,41 +9,65 @@ namespace Bank_Heist_2
         {
             Console.WriteLine("Hello World!");
 
-
             Hacker matt = new Hacker();
             LockSpecialist lockPickingLawyer = new LockSpecialist();
             Muscle arnold = new Muscle();
             Hacker mrRobot = new Hacker();
             Muscle leith = new Muscle();
 
-
             List<IRobber> rolodex = new List<IRobber>()
             {
                 matt, lockPickingLawyer, arnold, mrRobot, leith
             };
 
-            Console.WriteLine($"Number of Operatives: {rolodex.Count}");
+            while (true)
+            {
+                Console.WriteLine($"Number of Operatives: {rolodex.Count}");
 
-            Console.WriteLine($"Enter the Name of a New Crew Member:");
+                Console.Write($"Enter the Name of a New Crew Member: ");
 
-            string operativeName = Console.ReadLine();
+                string operativeName = Console.ReadLine();
 
-            Console.WriteLine($"Please choose an Operative: 1-Hacker(Disables alarms), 2-Muscle(Disarms Guards), 3-Lock Specialist(Cracks Vault)");
+                if (operativeName == "")
+                {
+                    break;
+                }
 
-            int operativeChoice = int.Parse(Console.ReadLine());
+                Console.Write($"Please choose an Operative: 1-Hacker(Disables alarms), 2-Muscle(Disarms Guards), 3-Lock Specialist(Cracks Vault): ");
 
-            Console.WriteLine("Enter crew members skill level (0-100): ");
+                int operativeChoice = int.Parse(Console.ReadLine());
 
-            int operativeSkillLevel = int.Parse(Console.ReadLine());
+                Console.Write("Enter crew members skill level (0-100): ");
 
-            Console.WriteLine("Enter percentage cut for operative(0-100): ");
+                int operativeSkillLevel = int.Parse(Console.ReadLine());
 
-            int operativePercentage = int.Parse(Console.ReadLine());
+                Console.Write("Enter percentage cut for operative(0-100): ");
 
+                int operativePercentage = int.Parse(Console.ReadLine());
 
-            rolodex = AddOperative(operativeChoice, operativeName, operativeSkillLevel, operativePercentage, rolodex);
+                rolodex = AddOperative(operativeChoice, operativeName, operativeSkillLevel, operativePercentage, rolodex);
+            }
+
+            Random random = new Random();
+
+            Bank newBank = new Bank
+            {
+                AlarmScore = random.Next(0, 101),
+                VaultScore = random.Next(0, 101),
+                SecurityGuardScore = random.Next(0, 101),
+                CashOnHand = random.Next(50_000, 100_000_1),
+            };
+
+            string mostSecure = newBank.MostSecureSystem;
+            string leastSecure = newBank.LeastSecureSystem;
+
+            Console.WriteLine($"The most secure system is {mostSecure}");
+
+            Console.WriteLine($"The least secure system is {leastSecure}");
+
 
         }
+
 
         public static List<IRobber> AddOperative(int choice, string name, int skillLevel, int percentageCut, List<IRobber> rolodex)
         {
